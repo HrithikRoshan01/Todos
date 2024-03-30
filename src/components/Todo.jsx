4e6503adcd2a968fb37c7a5919f3d3c27a24cb3d
Hrithik Roshan <163551846+HrithikRoshan01@users.noreply.github.com>
2024-03-31 01:47:34 +0530

    responsive

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,34 +1,25 @@
-import React,{useState} from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { removeTodo } from '../features/todo/todoSlice'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeTodo } from '../features/todo/todoSlice';
 
-function Todo(){
-    
+function Todo() {
+    const todos = useSelector((state) => state.todos);
+    const dispatch = useDispatch();
 
-    const todos =  useSelector((state) => state.todos)
-    const dispatch = useDispatch()
     return (
-         <>
-         
-        <div className='p-4 m-4 mx-80'>
+        <div className="p-4 m-4 mx-auto md:w-3/4 lg:w-1/2">
             <ul className="list-none">
                 {todos.map((todo) => (
-                    
-                    <li style={{ height: `${Math.max(todo.text.length * 1, 40)}px` }} 
-                        className=' bg-gradient-to-r from-cyan-500 to-blue-500 m-2 px-4 flex justify-between rounded-lg h-10 items-center ' key = {todo.id}>
-                        {todo.text}
-                        <button
-                        // className='m-4'
-                        onClick={()=>dispatch(removeTodo(todo.id))}
-                        >   ❌ </button>
-                        {/* <button onClick={AddClass} className='border border-gray-300   dark:bg-green-700'  >Update</button> */}
-                        
-                    </li>   
+                    <li key={todo.id} className="todo-item">
+                        <div className="flex justify-between items-center bg-gradient-to-r from-cyan-500 to-blue-500 m-2 px-4 py-2 rounded-lg">
+                            <span>{todo.text}</span>
+                            <button onClick={() => dispatch(removeTodo(todo.id))}>❌</button>
+                        </div>
+                    </li>
                 ))}
             </ul>
-         </div>
-         </>
-    )
+        </div>
+    );
 }
 
-export default Todo
+export default Todo;
